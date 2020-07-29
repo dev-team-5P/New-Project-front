37338bb773f09064d116f972d8b3211921ffc915
AA-BENTOUMI <amenbentoumi@gmail.com>
2020-07-29 14:55:14 +0200

    filter and seach

--- src/app/views/superadmin/listetablissement/listetablissement.component.ts
+++ src/app/views/superadmin/listetablissement/listetablissement.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ViewChild } from '@angular/core';
 import { SuperadminService } from '../../../services/superadmin.service';
 import * as jwt_decode from 'jwt-decode';
-
+import {MatPaginator} from '@angular/material/paginator';
+import {MatTableDataSource} from '@angular/material/table';
+import {MatSortModule} from '@angular/material/sort';
 export interface Data {
   nom: string;
   adresse: string;
@@ -17,23 +19,39 @@ const ELEMENT_DATA: Data[] = [
   styleUrls: ['./listetablissement.component.css']
 })
 export class ListetablissementComponent implements OnInit {
-  displayedColumns: string[] = ['nom', 'adresse', 'tel', 'fax'];
-  
-  dataSource;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSortModule, {static: false}) sort: MatSortModule;
+  displayedColumns: string[] = ['nom', 'adresse', 'tel', 'fax', 'Edit','Delete'];
+  dataSource: any; ;
 
   constructor(private adminservice: SuperadminService,) { }
 
   ngOnInit(): void {
     this.getalletab();
+    
   }
   // ************* get all etabllisement for superAdmin*******//
-  getalletab() {
-    // if (this.decoded.data.role === 'superAdmin') 
-    {
-      this.adminservice.getall().subscribe(data=> {
-          this.dataSource =data;
-          console.log(this.dataSource)
-        });
-    } 
+  getalletab() {  
+    this.adminservice.getall()  
+      .subscribe(  
+      res => {  
+        this.dataSource = new MatTableDataSource();  
+        this.dataSource.data = res;  
+        this.dataSource.paginator = this.paginator;  
+        console.log(this.dataSource.data);  
+      },  
+      error => {  
+        console.log('There was an error while retrieving data !!!' + error);  
+      });  
+  }  
+
+  Filter(searchstring:string)  
+  {  
+    searchstring = searchstring.trim();   
+    searchstring = searchstring.toLowerCase();  
+    this.dataSource.filter = searchstring;  
+  }
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
   }
 }
