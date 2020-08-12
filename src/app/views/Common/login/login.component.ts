f76abbdc7228e38afb17886ec60d6291a0323803
beymedhabib <beymedhabib@gmail.com>
2020-08-12 10:19:57 +0100

    avatar async

--- src/app/views/Common/login/login.component.ts
+++ src/app/views/Common/login/login.component.ts
@@ -3,6 +3,8 @@ import { FormGroup, FormControl, Validators} from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../../../services/auth.service';
 import {ToasterModule, ToasterService} from 'angular2-toaster';
+import * as jwt_decode from 'jwt-decode';
+
 
 @Component({
   selector: 'app-login',
@@ -13,6 +15,8 @@ export class LoginComponent implements OnInit {
   // private toasterService: ToasterService;
   LoginForm: FormGroup; 
   hide = true;
+  decoded: any;
+
   constructor(    
     private auth: AuthService,
     private router: Router,
@@ -28,6 +32,15 @@ export class LoginComponent implements OnInit {
   this.auth.signin(this.LoginForm.value).subscribe(
     (res: any) => {
       localStorage.setItem("token", res.message);
+      this.decoded = jwt_decode(res.message);
+      if (this.decoded.data.logo != undefined)
+      {
+        localStorage.setItem('avatar', this.decoded.data.logo);
+      }
+      else {
+        localStorage.setItem('avatar', 'assets/img/avatars/5.jpg');
+      }
+     
       this.toasterService.pop('success', 'success', 'Welcom back');
       this.router.navigateByUrl("/dashboard");
     },
